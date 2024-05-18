fe68c4c1fdf4c2e304344f26ab0e9b3618e52c0e
Arun Joseph <arunjoseph3007@gmail.com>
2024-05-18 17:36:43 +0530

    Beziers First iteration
    Not symmetrical, ugly and inaccurate but still working

--- glyph.py
+++ glyph.py
@@ -1,4 +1,6 @@
 from typing import List, Tuple
+
+import pygame.gfxdraw
 from utils import isNthBitOn
 import pygame
 
@@ -25,17 +27,40 @@ class Glyph:
         ]
 
     def draw(self, screen: pygame.Surface, loc: Tuple[int, int]):
+        steps = 20
+        blue = (0, 0, 255)
+        green = (0, 255, 0)
         locX, locY = loc
         newPoints = [(x * 0.2 + locX, 300 - y * 0.2 + locY) for x, y in self.points]
 
         for i, p in enumerate(newPoints):
             onTheCurve = isNthBitOn(self.flags[i], 0)
-            pygame.draw.circle(screen, (0, 0, 255) if onTheCurve else (0, 255, 0), p, 3)
+            pygame.draw.circle(screen, blue if onTheCurve else green, p, 3)
 
         startIndex = 0
 
+        spline: List[Tuple[int, int]] = []
         for endIndex in self.endPtsOfContours:
-            pygame.draw.polygon(
-                screen, (0, 0, 255), newPoints[startIndex : endIndex + 1], width=1
-            )
+            spline = []
+            for _i, p in enumerate(newPoints[startIndex : endIndex + 1]):
+                i = _i + startIndex
+                onTheCurve = isNthBitOn(self.flags[i], 0)
+                if not onTheCurve:
+                    spline.append(p)
+                elif len(spline) == 1:
+                    pygame.draw.line(screen, blue, spline[0], p)
+                    spline = [p]
+                elif len(spline) > 1:
+                    spline.append(p)
+                    pygame.gfxdraw.bezier(screen, spline, steps, blue)
+                    spline = [p]
+                else:
+                    spline.append(p)
+            # Last countour with starting point
+            if len(spline) == 1:
+                pygame.draw.line(screen, blue, spline[0], newPoints[startIndex])
+            else:
+                spline.append(newPoints[startIndex])
+                pygame.gfxdraw.bezier(screen, spline, steps, blue)
+
             startIndex = endIndex + 1
