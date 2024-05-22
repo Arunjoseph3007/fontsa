71a98c556cb149119035fb1a5bce6c611ac73f3c
Arun Joseph <arunjoseph3007@gmail.com>
2024-05-22 16:16:30 +0530

    Colors from global styles

---
+++ styles.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Colors(Enum):
+    BackGround = (10, 6, 8)
+    Text = (239, 231, 236)
+    Primary = (200, 173, 188)
+    Secondary = (96, 65, 70)
+    Accent = (172, 131, 132)
